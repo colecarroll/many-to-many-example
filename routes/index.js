4a891831a7eaddb7bb60d1211d85f8c2cff61cd9
coloradokim <schlesinger.kim@gmail.com>
2017-11-08 11:44:50 -0700

    update seeds

--- routes/index.js
+++ routes/index.js
@@ -18,20 +18,22 @@ router.get('/movies', function(req, res, next) {
 });
 
 router.get('/movies/:id', function(req, res, next) {
-  // write your code here
-
-  // .then((movieInfo) => {
-  //   res.status(201).json({
-  //     status: 'success',
-  //     data: movieInfo
-  //   });
-  // })
-  // .catch((err) => {
-  //   res.status(500).json({
-  //     status: 'error',
-  //     data: err.message
-  //   });
-  // });
+  dbConnection('movie').where('movie.id', req.params.id)
+  .select('movie.title', 'actor.name')
+  .innerJoin('actor_movie', 'movie.id', 'actor_movie.movie_id')
+  .innerJoin('actor', 'actor.id', 'actor_movie.actor_id')
+  .then((movieInfo) => {
+    res.status(201).json({
+      status: 'success',
+      data: movieInfo
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error',
+      data: err.message
+    });
+  });
 });
 
 module.exports = router;
